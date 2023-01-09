fc46faf08d91adcd00a857dc8612203dba4a3e05
Joscha von Hein <joscha.vonhein@cimt-ag.de>
2023-01-09 21:17:22 +0100

    errors done

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -36,7 +36,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green = ColorTupleStruct(0, 255, 0)
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
